720e3f1043e8cd8c8479ca0f5da7e69fa78d0a56
Yasu <blackcat7134@gmail.com>
2024-12-17 09:39:58 +0800

    navJoson

--- custom/js/nav.js
+++ custom/js/nav.js
@@ -1,33 +1,44 @@
-let navData=[{
-    navList:"About",
+let showData = {
+[
+  {
+    showTitle: ["Our Story", "News", "Why Choose HMS"],
+    showList: ["HMS Brief intro", "Core Competency", "Contact"],
+  },
+  {
+    showTitle: "News",
+    showList: ["參展 / 獲獎消息", "Gallery"],
+  },
+  {
+    showTitle: "Why Choose HMS",
+    showList: ["Your Smart Healthcare Solution Partner", "AI Real-Time Breathing Monitoring System"],
+  },
+],
+[
+    {
+      showTitle: ["Our Story", "News", "Why Choose HMS"],
+      showList: ["HMS Brief intro", "Core Competency", "Contact"],
+    },
+    {
+      showTitle: "News",
+      showList: ["參展 / 獲獎消息", "Gallery"],
+    },
+    {
+      showTitle: "Why Choose HMS",
+      showList: ["Your Smart Healthcare Solution Partner", "AI Real-Time Breathing Monitoring System"],
+    },
+  ],
 },
-{
-    navList:"Solution",
-},
-{
-    navList:"Contact",
-}, 
-]
 
+// const nav = document.querySelector(`.mainNav`);
 
-const nav = document.querySelector(".mainNav");
-function init(){
-    let str = ""
-    navData.forEach(function(item,index){
-console.log(item.navList);
-let content = `<li class="px-3 navLink ">
-                                <a href="#" style="display: flex;width: 100%;">${item,navList}</a>
-                            </li>`
-                            str+='content';
-    })
-    list.innerHTML = str ;
-}
-init();
-// console.log(nav)
-
-nav.addEventListener("click",function(e){
-    // console.log(e.target.value);
-    if(e.target.value == undefined){
-        console.log("點歪");
-    }
-})
+// function init() {
+//   let str = "";
+//   navData.forEach(function (item, index) {
+//     let content = `<li class="px-3" >
+//                                 <a href="#" style="display: flex;width: 100%">${item.navTitle}</a>
+//                             </li>`;
+//     str += content;
+//   });
+//   nav.innerHTML = str;
+// }
+// init();
